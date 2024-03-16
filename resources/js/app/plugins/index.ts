659eeaafd6cf1423fbc0f2487b3edc140fde0607
igor-yuzkiv <igor97w@gmail.com>
2024-03-16 10:35:54 +0200

    install

--- resources/js/app/plugins/index.ts
+++ resources/js/app/plugins/index.ts
@@ -1,6 +1,5 @@
 import {App} from "vue";
-import PrimeVue from 'primevue/config';
 
 export function registryPlugins (app: App) {
-    app.use(PrimeVue)
+
 }
---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+    content: [
+        './index.html',
+        './resources/js/**/*.{js,jsx,ts,tsx}',
+        './resources/js/**/*.vue'
+    ],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+}
+
