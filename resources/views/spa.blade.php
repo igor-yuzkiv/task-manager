88eda102a1a5388b424f4dce786fd12b6486d08a
Igor Yuzkiv <igor97w@gmail.com>
2024-03-17 15:59:01 +0200

    font

--- resources/views/spa.blade.php
+++ resources/views/spa.blade.php
@@ -6,12 +6,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>{{config('app.name')}}</title>
-    <link rel="preconnect" href="https://fonts.googleapis.com" />
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-    <link
-        rel="stylesheet"
-        href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700&display=swap"
-    />
 </head>
 <body>
 <div id="app"></div>
