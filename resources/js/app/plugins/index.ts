0f02b19e79a5fe348a762c3fb0f552cf82725ff9
Igor Yuzkiv <igor97w@gmail.com>
2024-03-17 15:41:10 +0200

    install pinia

--- resources/js/app/plugins/index.ts
+++
@@ -1,6 +0,0 @@
-import {App} from "vue";
-import {router} from "@/app/router";
-
-export function registryPlugins(app: App) {
-    app.use(router);
-}
--- resources/js/main.ts
+++ resources/js/main.ts
@@ -1,10 +1,13 @@
 import {createApp} from 'vue'
 import './style.css'
 import App from './app/App.vue'
-import {registryPlugins} from "@/app/plugins";
+import { createPinia } from 'pinia'
+import {router} from "@/app/router";
 
+const pinia = createPinia();
 const app = createApp(App)
 
-registryPlugins(app);
+app.use(pinia);
+app.use(router);
 
 app.mount('#app')
